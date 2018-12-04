33a1ed546449d691c7e781fbf45b483acbb0ca42
Carlos Blanco <carlos.blanco.2901@gmail.com>
2018-12-04 15:58:24 -0400

    Mejoras en Call Center

--- GenerateDataCallCenter.go
+++ GenerateDataCallCenter.go
@@ -52,6 +52,9 @@ func isDAte2(s string) bool {
 }
 
 func main() {
+
+	fecha_actual := time.Now()
+
 	directory := "/home/taksioweb-01/CallCenter/"
 
 	dbinfo := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -63,12 +66,6 @@ func main() {
 	}
 	defer db.Close()
 
-	_, err1 := db.Exec("TRUNCATE TABLE callcenter.callcenterdata RESTART IDENTITY")
-	if err1 != nil {
-		db.Close()
-		panic(err1)
-	}
-
 	files, err2 := ioutil.ReadDir(directory)
 	if err2 != nil {
 		db.Close()
@@ -76,35 +73,50 @@ func main() {
 	}
 	for _, file := range files {
 		excelFileName := directory + file.Name()
-		xlFile, err3 := xlsx.OpenFile(excelFileName)
-		if err3 != nil {
-			db.Close()
-			panic(err3)
-		}
-		for _, sheet := range xlFile.Sheets {
-			if isDAte(strings.TrimSpace(sheet.Name)) {
-				for _, row := range sheet.Rows {
-					if strings.TrimSpace(row.Cells[1].String()) != "" && isDAte2(strings.TrimSpace(row.Cells[1].String())) {
-						if strings.ToUpper(strings.TrimSpace(row.Cells[6].String())) != "APP" && strings.TrimSpace(row.Cells[2].String()) != "" {
-							driver := strings.ToUpper(row.Cells[2].String())
-							driver = strings.Replace(driver, "Á", "A", -1)
-							driver = strings.Replace(driver, "É", "E", -1)
-							driver = strings.Replace(driver, "Í", "I", -1)
-							driver = strings.Replace(driver, "Ó", "O", -1)
-							driver = strings.Replace(driver, "Ú", "U", -1)
-							//println(row.Cells[1].String() + " " + row.Cells[2].String() + " " + row.Cells[3].String() + " " + row.Cells[4].String() + " " + row.Cells[5].String() + " " + row.Cells[5].String() + " " + row.Cells[9].String())
-							//println(row.Cells[6].String())
-							_, err4 := db.Exec("INSERT INTO callcenter.callcenterdata(date_ride, driver, origin, destination, rider, tks) VALUES (to_date('" + strings.TrimSpace(row.Cells[1].String()) + "','DD/MM/YYYY'), '" + strings.TrimSpace(driver) + "', '" + strings.TrimSpace(row.Cells[3].String()) + "', '" + strings.TrimSpace(row.Cells[4].String()) + "', 'a8a90dd8-02fd-4f86-815b-90cab0435d46', '" + strings.TrimSpace(row.Cells[9].String()) + "');")
-							if err4 != nil {
-								db.Close()
-								panic(err4)
+		var nombre_archivo = file.Name()
+		if !strings.Contains(nombre_archivo, "**") {
+			xlFile, err3 := xlsx.OpenFile(excelFileName)
+			if err3 != nil {
+				db.Close()
+				panic(err3)
+			}
+			for _, sheet := range xlFile.Sheets {
+				if isDAte(strings.TrimSpace(sheet.Name)) {
+					fecha_titulo, _ := time.Parse("02-01-2006", strings.TrimSpace(sheet.Name))
+
+					fmt.Println(fecha_titulo.Format("02-01-2006"))
+
+					if fecha_titulo.Format("02-01-2006") != fecha_actual.Format("02-01-2006") {
+						_, err = db.Exec("DELETE FROM callcenter.callcenterdata WHERE date_ride = '" + fecha_titulo.Format("2006-01-02") + "'")
+						if err != nil {
+							db.Close()
+							panic(err)
+						}
+
+						for _, row := range sheet.Rows {
+							if strings.TrimSpace(row.Cells[1].String()) != "" && isDAte2(strings.TrimSpace(row.Cells[1].String())) {
+								if strings.ToUpper(strings.TrimSpace(row.Cells[6].String())) != "APP" && strings.TrimSpace(row.Cells[2].String()) != "" {
+									driver := strings.ToUpper(row.Cells[2].String())
+									driver = strings.Replace(driver, "Á", "A", -1)
+									driver = strings.Replace(driver, "É", "E", -1)
+									driver = strings.Replace(driver, "Í", "I", -1)
+									driver = strings.Replace(driver, "Ó", "O", -1)
+									driver = strings.Replace(driver, "Ú", "U", -1)
+									//println(row.Cells[1].String() + " " + row.Cells[2].String() + " " + row.Cells[3].String() + " " + row.Cells[4].String() + " " + row.Cells[5].String() + " " + row.Cells[5].String() + " " + row.Cells[9].String())
+									//println(row.Cells[6].String())
+									_, err4 := db.Exec("INSERT INTO callcenter.callcenterdata(date_ride, driver, origin, destination, rider, tks) VALUES (to_date('" + strings.TrimSpace(row.Cells[1].String()) + "','DD/MM/YYYY'), '" + strings.TrimSpace(driver) + "', '" + strings.TrimSpace(row.Cells[3].String()) + "', '" + strings.TrimSpace(row.Cells[4].String()) + "', 'a8a90dd8-02fd-4f86-815b-90cab0435d46', '" + strings.TrimSpace(row.Cells[9].String()) + "');")
+									if err4 != nil {
+										db.Close()
+										panic(err4)
+									}
+								}
 							}
 						}
 					}
-
 				}
 			}
 		}
+
 	}
 	_, err5 := db.Exec("UPDATE callcenter.callcenterdata A SET driver = B.uuid FROM callcenter.userdata B WHERE A.driver = B.driver")
 	if err5 != nil {
